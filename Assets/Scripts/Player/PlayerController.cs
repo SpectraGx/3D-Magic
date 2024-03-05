c29d1f35822872d62d344bfddaa9b300bce89002
Fernando Mendoza <fernandogx8647@gmail.com>
2024-03-05 11:56:56 -0600

    Cam & Inputs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,31 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     //          VARIABLES           // 
-    [SerializeField] private float speed = 4;     //Control de velocidad
+    [SerializeField] private float speed = 4;
 
     private CharacterController characterController;    //Referencia a CharacterController
-    private Vector3 currentMovent;  // Dirección y magnitud del movimiento
-    private bool isMoving;      // Booleano que indica si el personaje se mueve
+    private PlayerInput playerInput;
+    private Rigidbody rb;
+    private Vector2 input;
+    private Vector3 currentMovent;
+    private bool isMoving;
 
     private void Awake()
     {
-        characterController = GetComponent<CharacterController>();  // Referencia a CharacterController
+        characterController = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     private void Update()
     {
         ControllerMovement();
+        //input = playerInput.actions["Move"].ReadValue<Vector2>(); 
+    }
+
+    private void FixedUpdate()
+    {
+        //rb.AddForce(new Vector3(input.x,0f,input.y)*speed);
     }
 
     private void ControllerMovement()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));    // Entradas de teclado
-        currentMovent = move.normalized * (Time.deltaTime * speed);     // Normaliza la velocidad por el tiempo
-        characterController.Move(currentMovent);    // Aplica movimiento
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        currentMovent = move.normalized * (Time.deltaTime * speed);
+        characterController.Move(currentMovent);
 
         // Control de la rotacion del personaje
         if (move != Vector3.zero)
@@ -40,6 +51,13 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    //  Metodo publico que indica si el personaje esta en movimiento o no
+    public void Cut(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.performed)
+        {
+            Debug.Log("Esta cortando");
+        }
+    }
+
     public bool IsMoving() => isMoving;
 }
