2ebfb7a86a8bb5311f0a57cce8af50e5136c2b8c
Fernando Mendoza <fernandogx8647@gmail.com>
2024-05-12 22:33:06 -0600

    Menus

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play(string name)
+    {
+        SceneManager.LoadScene(name);
+        Debug.Log(name);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Salir");
+    }
+
+    public void FullScreen(){
+        Screen.fullScreen = true;
+    }
+
+    public void Window(){
+        Screen.fullScreen = false;
+    }
+}
