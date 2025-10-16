f46bc23335b78e57c95d8e94bb026b318b260205
Fernando Mendoza <fernandogx8647@gmail.com>
2025-10-16 06:50:35 -0600

    RecetarioUI

--- Assets/Scripts/UI/DeliveryManagerSingleUI.cs
+++ Assets/Scripts/UI/DeliveryManagerSingleUI.cs
@@ -7,27 +7,15 @@ using UnityEngine.UI;
 public class DeliveryManagerSingleUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI potionNameText;
-    [SerializeField] private Transform iconContainer;
-    [SerializeField] private Transform iconTemplate;
+    [SerializeField] private Image iconPotion;
 
-    private void Awake() {
-        iconTemplate.gameObject.SetActive(false);
-    }
-
-    public void SetPotionData(IngredientData potionData){
+    public void SetPotionData(IngredientData potionData)
+    {
         potionNameText.text = potionData.itemName;
 
-        foreach(Transform child in iconContainer){
-            if(child == iconTemplate) continue;
-            Destroy(child.gameObject);
+        if (iconPotion != null)
+        {
+            iconPotion.sprite = potionData.itemSprite;
         }
-
-        foreach(IngredientData ingredientData in potionData.ingredientsDataList){
-            Transform iconTransform = Instantiate(iconTemplate,iconContainer);
-            iconTransform.gameObject.SetActive(true);
-            iconTransform.GetComponent<UnityEngine.UI.Image>().sprite = ingredientData.itemSprite;
-        }
-
-
     }
 }
